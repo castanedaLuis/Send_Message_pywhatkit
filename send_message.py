8fe634df3082e48bd666d85ae556338005809d63
Jose Luis Castañeda Osornio <78225172+castanedaLuis@users.noreply.github.com>
2022-10-20 18:50:37 -0500

    Update send_message.py
    Modificaciones

--- send_message.py
+++ send_message.py
@@ -3,15 +3,15 @@ import pywhatkit
 try: 
      
   # send message 
-  pywhatkit.sendwhatmsg("+522221818476","Hola mundo!",18, 46)#Time
+  pywhatkit.sendwhatmsg("+52**********","Hola mundo!",18, 46)#Time
   # Send an Image to a Group with the Caption as Hello
   pywhatkit.sendwhats_image("PruebaSend", "Images/Hello.png", "Hello")
   # Send an Image to a Contact with the no Caption
-  pywhatkit.sendwhats_image("+910123456789", "Images/Hello.png")
+  pywhatkit.sendwhats_image("+52**********", "Images/Hello.png")
   # Send a WhatsApp Message to a Group at 12:00 AM
-  pywhatkit.sendwhatmsg_to_group("AB123CDEFGHijklmn", "Hey All!", 0, 0)
+  pywhatkit.sendwhatmsg_to_group("GrupoPrueba", "Hey All!", 0, 0)
   # Send a WhatsApp Message to a Group instantly
-  pywhatkit.sendwhatmsg_to_group_instantly("AB123CDEFGHijklmn", "Hey All!") 
+  pywhatkit.sendwhatmsg_to_group_instantly("GrupoPrueba", "Hey All!") 
   print("Successfully Sent!")
    
 except: 
